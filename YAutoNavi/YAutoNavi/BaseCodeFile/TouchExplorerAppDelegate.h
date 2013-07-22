fc4ec6923e421239a71a1585647b1531c8f095da
yuhang.li <liyuhang06@163.com>
2013-07-22 14:54:27 +0800

    第一次版本低头角

---
+++ YAutoNavi/YAutoNavi/BaseCodeFile/TouchExplorerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TouchExplorerAppDelegate.h
+//  YAutoNavi
+//
+//  Created by liyuhang on 13-7-22.
+//  Copyright (c) 2013年 liyuhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouchExplorerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YAutoNavi/YAutoNavi/BaseCodeFile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YAutoNavi
+//
+//  Created by liyuhang on 13-7-22.
+//  Copyright (c) 2013年 liyuhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TouchExplorerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TouchExplorerAppDelegate class]));
+    }
+}
